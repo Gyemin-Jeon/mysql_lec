90502c2d5e2559337f9e0266f3740a064377068d
Gyemin-Jeon <asdjeon@naver.com>
2024-11-22 13:08:16 +0900

    20241122

--- 04.모델링.sql
+++ 04.모델링.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS tbl_department
 
 CREATE TABLE IF NOT EXISTS tbl_project
 (
-    proj_id     INT NOT NULL COMMENT '프로젝트아이디',
+    proj_id     INT NOT NULL AUTO_INCREMENT COMMENT '프로젝트아이디',
     proj_name   VARCHAR(30) COMMENT '프로젝트명',
     begin_date  DATE COMMENT '시작일자',
     end_date    DATE COMMENT '종료일자',
@@ -38,10 +38,10 @@ CREATE TABLE IF NOT EXISTS tbl_project
 
 CREATE TABLE IF NOT EXISTS tbl_employee
 (
-    emp_id      INT NOT NULL COMMENT '사원아이디',
+    emp_id      INT NOT NULL AUTO_INCREMENT COMMENT '사원아이디',
     dept_id     INT COMMENT '부서아이디',
-    position    CHAR(10) COMMENT '직급',
     emp_name    VARCHAR(15) COMMENT '사원명',
+    position    CHAR(10) COMMENT '직급',
     gender      CHAR(1) COMMENT '성별',
     hire_date   DATE COMMENT '입사일자',
     salary      INT COMMENT '연봉',
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS tbl_employee
 
 CREATE TABLE IF NOT EXISTS tbl_proj_emp
 (
-    reg_id      INT NOT NULL COMMENT '등록아이디',
+    reg_id      INT NOT NULL AUTO_INCREMENT COMMENT '등록아이디',
     emp_id      INT COMMENT '사원아이디',
     proj_id     INT COMMENT '프로젝트아이디',
     state       INT NOT NULL COMMENT '진행상태',
